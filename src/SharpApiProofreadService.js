3aad59c650fb0a4148de2681cbf3f571d0352689
Dawid Makowski <dawid.makowski@gmail.com>
2026-01-09 23:33:52 +0700

    Update to use NPM core package and add custom User-Agent
    - Changed @sharpapi/sharpapi-node-core dependency from local file to ^1.0.0
    - Added custom User-Agent: @sharpapi/sharpapi-node-proofread/1.0.1
    - Bumped version to 1.0.1

--- src/SharpApiProofreadService.js
+++ src/SharpApiProofreadService.js
@@ -5,6 +5,15 @@ const { SharpApiCoreService, SharpApiJobTypeEnum } = require('@sharpapi/sharpapi
  */
 class SharpApiProofreadService extends SharpApiCoreService {
   /**
+   * Creates a new SharpApiProofreadService instance
+   * @param {string} apiKey - Your SharpAPI API key
+   * @param {string} [apiBaseUrl='https://sharpapi.com/api/v1'] - API base URL
+   */
+  constructor(apiKey, apiBaseUrl = 'https://sharpapi.com/api/v1') {
+    super(apiKey, apiBaseUrl, '@sharpapi/sharpapi-node-proofread/1.0.1');
+  }
+
+  /**
    * Proofreads (and checks grammar) of the provided text.
    *
    * @param {string} text
